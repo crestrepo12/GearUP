71ef22a41a6a0509716e8c730bc993a940c31298
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-06-05 11:02:11 -0400

    added functions for the accordion menu

--- backend/client/src/components/providers/JourneyTrack.jsx
+++ backend/client/src/components/providers/JourneyTrack.jsx
@@ -3,15 +3,37 @@ import { Link, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import { Accordion, Form, Menu, Header } from "semantic-ui-react";
 
+function Objectives(props) {
+return (
+    <Form>
+      <Form.Group grouped>
+        <Form.Checkbox label={props.client.firstname} name="color" value="red" />
+        <Form.Checkbox label={props.client.lastname} name="color" value="orange" />
+        <Form.Checkbox label={props.client.age} name="color" value="green" />
+        <Form.Checkbox label={props.client.occupation} name="color" value="blue" />
+      </Form.Group>
+    </Form>
+  )
+}
+
 class JourneyTrack extends Component {
   constructor() {
     super();
 
     this.state = {
       life_skills: [],
-      client: {}
+      client: {},
+      activeIndex: 0
     };
   }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
 
   getClient = () => {
     //fetches one client
@@ -51,17 +73,18 @@ class JourneyTrack extends Component {
       });
   };
 
-  //   getAllObjectivesListFromLifeSkills = () => {
-
-  //   }
+  getAllObjectivesList = () => {
+    // get all objectives of under life skills categories depending on current client (id) page
+  };
 
   componentDidMount() {
     this.getClient();
     this.getAllLifeSkillCategories();
+    // this.getAllObjectivesList();
   }
 
   render() {
-    const { life_skills, client } = this.state;
+    const { life_skills, client, activeIndex } = this.state;
 
     return (
       <div id="journey-track" className="margin-top">
@@ -69,16 +92,23 @@ class JourneyTrack extends Component {
           {`${client.firstname} ${client.lastname}'s Journey`}
         </Header>
         <div id="journey-container">
-        {life_skills.map(skill => {
-          return (
-            <Accordion as={Menu} vertical fluid
-            >
-              <Menu.Item>
-                <Accordion.Title content={skill.categories} />
-              </Menu.Item>
-            </Accordion>
-          );
-        })}
+          {life_skills.map((skill, idx) => {
+            return (
+              <Accordion as={Menu} vertical fluid>
+                <Menu.Item>
+                  <Accordion.Title
+                    content={skill.categories}
+                    active={activeIndex === idx}
+                    index={idx}
+                    onClick={this.handleClick}
+                  />
+                  <Accordion.Content active={activeIndex === idx} >
+                    <Objectives client={client} />
+                 </Accordion.Content>
+                </Menu.Item>
+              </Accordion>
+            );
+          })}
         </div>
       </div>
     );
