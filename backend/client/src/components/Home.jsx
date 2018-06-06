ff2512d077dea03fb94008ca11c2b036b8dd5495
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-06-06 12:27:03 -0400

    change home container to add a tagline

--- backend/client/src/components/Home.jsx
+++ backend/client/src/components/Home.jsx
@@ -10,14 +10,17 @@ class Home extends Component {
   render() {
     return (
       <div className="home-page yellow full-height fifty-width">
-        <Header as="h2" className="center">Gear UP</Header>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu
-          lobortis elementum nibh. Pretium viverra suspendisse potenti nullam ac
-          tortor vitae purus. Netus et malesuada fames ac turpis egestas.
-        </p>
+        {/* <Header as="h2" className="center">
+          Gear UP
+        </Header> */}
+        <div className="tagline">
+          <Header as="h1" >
+            Modernizing Special Needs
+          </Header>
+          <Header as="h1" >
+            Case Management
+          </Header>
+        </div>
       </div>
     );
   }
