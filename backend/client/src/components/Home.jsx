055d1546969bd9b3432a66a07e6e7e65715b02f2
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-08-12 23:07:18 -0400

    updated register form to be more accessible friendly

--- backend/client/src/components/Home.jsx
+++ backend/client/src/components/Home.jsx
@@ -10,12 +10,9 @@ class Home extends Component {
   render() {
     return (
       <div className="home-page full-height fifty-width">
-        {/* <Header as="h2" className="center">
-          Gear UP
-        </Header> */}
         <div className="tagline circle-block" >
           <Header as="h1" color="white">
-            Modernizing Special Needs Case Management.
+            Modernizing Special Needs Case Management
           </Header>
         </div>
       </div>
